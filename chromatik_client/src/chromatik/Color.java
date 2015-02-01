de9a162b60321b7b71c74480932d73132dea384a
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-02-01 02:47:55 +0100

    Chromatik connector library
     - Approximate arbitrary colors to preset colors:
       - select "column" bin by hue,
       - select "row" bin by euklidian distance in saturation-brightness
         vector space.
     - Store parameters in query object that can assemble query strings.

---
+++ chromatik_client/src/chromatik/Color.java
@@ -0,0 +1,125 @@
+package chromatik;
+
+import static java.lang.Math.max;
+import static java.lang.Math.round;
+import static chromatik.Utils.square;
+
+
+public class Color
+{
+  public final int value;
+
+  public final String groupName;
+
+  public Color( int rgb )
+  {
+    this((rgb >>> 16) & 0xff, (rgb >>> 8) & 0xff, rgb & 0xff);
+  }
+
+  public Color( int r, int g, int b )
+  {
+    float[] hsb = java.awt.Color.RGBtoHSB(r, g, b, null);
+
+    if (hsb[1] < SATURATION_BINS[0] / (255 * 2))
+    {
+      // color is so desaturated that we'll map it to a gray scale color.
+
+      /*
+       * Find brightness bin index (black = 0, white = hueNames.length-1).
+       * Chromatik's darkest accepted gray value is 21 (roughly 255/12).
+       */
+      int brBin = max(round(hsb[2] * HUE_COLS), 1);
+
+      // and go back to gray value
+      int c = brBin * 255 / HUE_COLS;
+      assert c >= 21 && c <= 255;
+      value = c | c << 8 | c << 16;
+
+      // find color group name based on rules
+      groupName =
+        GRAYSCALE_NAMES[ (brBin <= 1) ? 0 : (brBin < HUE_COLS - 1) ? 1 : 2 ];
+    }
+    else
+    {
+      /*
+       * Round to a bin and wrap around hue value (so that pinkish red maps to
+       * red at bin 0).
+       */
+      int hueBin = round(hsb[0] * HUE_COLS) % HUE_COLS;
+
+      /*
+       * The row of in Chromatik's color selector is the bin index with the
+       * closest distance in the vector space of saturation and brightness.
+       */
+      float sat = hsb[1] * 255, br = hsb[2] * 255;
+      float min = Float.POSITIVE_INFINITY;
+      int row = -1;
+      for (int i = SATURATION_BINS.length - 1; i >= 0; i--) {
+        float d =
+          square(sat - SATURATION_BINS[i]) +
+          square(br - BRIGHTNESS_BINS[i]);
+        if (d < min) {
+          min = d;
+          row = i;
+        }
+      }
+
+      value = COLORS[row * HUE_COLS + hueBin];
+      groupName = HUE_NAMES[hueBin];
+    }
+  }
+
+  @Override
+  public int hashCode()
+  {
+    return value;
+  }
+
+  @Override
+  public boolean equals( Object obj )
+  {
+    return obj instanceof Color && ((Color) obj).value == this.value;
+  }
+
+  static final String[] HUE_NAMES = {
+    "Red", "Orange", "Yellow", "Green", "Green", "Green",
+    "Cyan", "Blue", "Blue", "Purple", "Pink", "Pink"
+  };
+
+  static final String[] GRAYSCALE_NAMES = {
+    "Black", "Gray", "White"
+  };
+
+  static final int HUE_COLS = HUE_NAMES.length;
+
+  static final float[] SATURATION_BINS = {
+    52, 107, 166, 227, 227, 228, 228
+  };
+
+  static final float[] BRIGHTNESS_BINS = {
+    248, 242, 235, 229, 172, 114, 57
+  };
+
+  static final int[] COLORS = {
+    0xf8c5c5, 0xf8dfc5, 0xf8f8c5, 0xdff8c5, 0xc5f8c5, 0xc5f8df,
+    0xc5f8f8, 0xc5dff8, 0xc5c5f8, 0xdfc5f8, 0xf8c5f8, 0xf8c5df,
+
+    0xf28c8c, 0xf2bf8c, 0xf2f28c, 0xbff28c, 0x8cf28c, 0x8cf2bf,
+    0x8cf2f2, 0x8cbff2, 0x8c8cf2, 0xbf8cf2, 0xf28cf2, 0xf28cbf,
+
+    0xeb5252, 0xeb9f52, 0xebeb52, 0x9feb52, 0x52eb52, 0x52eb9f,
+    0x52ebeb, 0x529feb, 0x5252eb, 0x9f52eb, 0xeb52eb, 0xeb529f,
+
+    0xe51919, 0xe57f19, 0xe5e519, 0x7fe519, 0x19e519, 0x19e57f,
+    0x19e5e5, 0x197fe5, 0x1919e5, 0x7f19e5, 0xe519e5, 0xe5197f,
+
+    0xac1313, 0xac5f13, 0xacac13, 0x5fac13, 0x13ac13, 0x13ac5f,
+    0x13acac, 0x135fac, 0x1313ac, 0x5f13ac, 0xac13ac, 0xac135f,
+
+    0x720c0c, 0x723f0c, 0x72720c, 0x3f720c, 0x0c720c, 0x0c723f,
+    0x0c7272, 0x0c3f72, 0x0c0c72, 0x3f0c72, 0x720c72, 0x720c3f,
+
+    0x390606, 0x391f06, 0x393906, 0x1f3906, 0x063906, 0x06391f,
+    0x063939, 0x061f39, 0x060639, 0x1f0639, 0x390639, 0x39061f
+  };
+}
---
+++ chromatik_client/src/chromatik/Utils.java
@@ -0,0 +1,25 @@
+package chromatik;
+
+import java.util.Arrays;
+
+
+public final class Utils
+{
+  private Utils() { }
+
+  public static float square( float x )
+  {
+    return x * x;
+  }
+
+  public static char[] toHex( long n, char[] dst )
+  {
+    if (dst == null)
+      dst = new char[Long.SIZE / 4];
+    int i;
+    for (i = dst.length - 1; i >= 0 && n != 0; i--, n >>>= 4)
+      dst[i] = Character.forDigit((int) n & 0xf, 16);
+    Arrays.fill(dst, 0, i + 1, '0');
+    return dst;
+  }
+}
