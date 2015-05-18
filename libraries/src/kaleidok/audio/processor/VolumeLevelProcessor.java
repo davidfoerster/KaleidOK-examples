e7f85a52abd0a949b6b041923e218163ea483332
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-05-18 20:41:19 +0200

    Implement volume level processor

---
+++ libraries/src/kaleidok/audio/processor/VolumeLevelProcessor.java
@@ -0,0 +1,32 @@
+package kaleidok.audio.processor;
+
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+
+
+public class VolumeLevelProcessor implements AudioProcessor
+{
+  private double level = Double.NaN;
+
+  public double getLevel()
+  {
+    return level;
+  }
+
+  public boolean process( AudioEvent audioEvent )
+  {
+    float[] buf = audioEvent.getFloatBuffer();
+    /**
+     * We could simply use AudioEvent#getRMS() here, but it relies on a
+     * numerically unstable sum of squares implementation.
+     */
+    level = Math.sqrt(kaleidok.util.Math.sumOfSquares(buf) / buf.length);
+    return true;
+  }
+
+  @Override
+  public void processingFinished()
+  {
+    // nothing to do here
+  }
+}
