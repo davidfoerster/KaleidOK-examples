04be7e3beb4b8555c3ee0d81b21bae582bc8128d
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-05-25 23:43:32 +0200

    Implement TarsosDSP audio processor based on Minim's FFT class

---
+++ libraries/src/kaleidok/audio/processor/MinimFFTProcessor.java
@@ -0,0 +1,124 @@
+package kaleidok.audio.processor;
+
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import ddf.minim.analysis.FFT;
+import kaleidok.audio.spectrum.Spectrum;
+
+
+public class MinimFFTProcessor implements AudioProcessor, Spectrum
+{
+  private FFT fft = null;
+
+  private float sampleRate = 0;
+
+  private final float[] buffer;
+
+  private int avgType = 0, avgParam1, avgParam2;
+
+  public MinimFFTProcessor( int bufferSize )
+  {
+    buffer = new float[bufferSize];
+  }
+
+  public void noAverages()
+  {
+    avgType = 0;
+    updateAverages();
+  }
+
+  public void linAverages( int bands )
+  {
+    avgType = 1;
+    avgParam1 = bands;
+    updateAverages();
+  }
+
+  public void logAverages( int minBandwidth, int bandsPerOctave )
+  {
+    avgType = 2;
+    avgParam1 = minBandwidth;
+    avgParam2 = bandsPerOctave;
+    updateAverages();
+  }
+
+  private void updateAverages()
+  {
+    if (fft != null) {
+      switch (avgType) {
+      case 0:
+        fft.noAverages();
+        break;
+
+      case 1:
+        fft.linAverages(avgParam1);
+        break;
+
+      case 2:
+        fft.logAverages(avgParam1, avgParam2);
+        break;
+
+      default:
+        throw new Error("avgType has an unexpected value");
+      }
+    }
+  }
+
+  public boolean isReady()
+  {
+    return fft != null;
+  }
+
+  @Override
+  public boolean process( AudioEvent audioEvent )
+  {
+    if (fft == null) {
+      sampleRate = audioEvent.getSampleRate();
+      fft = new FFT(buffer.length, sampleRate);
+      updateAverages();
+    }
+
+    float[] audioBuffer = audioEvent.getFloatBuffer();
+    System.arraycopy(audioBuffer, 0, buffer, 0, audioBuffer.length);
+    fft.forward(buffer);
+    return true;
+  }
+
+  @Override
+  public void processingFinished()
+  {
+    // Nothing to do here
+  }
+
+  @Override
+  public float get( int n )
+  {
+    return fft.getAvg(n);
+  }
+
+  @Override
+  public int getSize()
+  {
+    return fft.timeSize();
+  }
+
+  public float getSampleRate()
+  {
+    return sampleRate;
+  }
+
+  public float getBin( float freq )
+  {
+    return fft.freqToIndex(freq);
+  }
+
+  public float getFreq( float bin )
+  {
+    return getFreq((int) bin);
+  }
+
+  public float getFreq( int bin )
+  {
+    return fft.getAverageCenterFrequency(bin);
+  }
+}
