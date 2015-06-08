561b41e4f772b2b4810e35a722adbd5f93f9257f
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-06-08 11:05:03 +0200

    Download image resources asynchronously (2/3)
    Enrich PApplet with convenience methods to create Futures of PImage.

--- libraries/src/kaleidok/processing/ExtPApplet.java
+++ libraries/src/kaleidok/processing/ExtPApplet.java
@@ -5,8 +5,11 @@ import kaleidok.util.DefaultValueParser;
 import processing.core.PApplet;
 
 import javax.swing.JApplet;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 /**
@@ -23,16 +26,51 @@ public class ExtPApplet extends PApplet
       DebugManager.fromApplet(parent);
   }
 
+
   @Override
   public String getParameter( String name )
   {
-    return (parent != null) ? parent.getParameter(name) : null;
+    return parent.getParameter(name);
   }
 
   public Object getParameter( String name, Object defaultValue )
   {
     return DefaultValueParser.parse(getParameter(name), defaultValue);
   }
+
+
+  private URL documentBase = null;
+
+  @Override
+  public URL getDocumentBase()
+  {
+    if (documentBase == null) {
+      URL documentBase = parent.getDocumentBase();
+      if (documentBase.getProtocol().equals("file")) {
+        documentBase = this.getClass().getProtectionDomain().getCodeSource().getLocation();
+        try {
+          documentBase = new URL(documentBase, documentBase.getPath() + "data/");
+        } catch (MalformedURLException ex) {
+          throw new Error(ex);
+        }
+      }
+      this.documentBase = documentBase;
+    }
+    return documentBase;
+  }
+
+  @Override
+  public Image getImage( URL url )
+  {
+    return parent.getImage(url);
+  }
+
+  @Override
+  public Image getImage( URL url, String name )
+  {
+    return parent.getImage(url, name);
+  }
+
 
   @Override
   public void keyPressed( KeyEvent e )
@@ -57,5 +95,32 @@ public class ExtPApplet extends PApplet
   public void keyTyped( KeyEvent e )
   {
     super.keyTyped(e);
+  }
+
+
+  public PImageFuture getImageFuture( String path )
+  {
+    URL url;
+    try {
+      url = new URL(path);
+    } catch (MalformedURLException ex) {
+      try {
+        url = new URL(getDocumentBase(), path);
+      } catch (MalformedURLException e) {
+        throw new IllegalArgumentException(ex);
+      }
+    }
+
+    return getImageFuture(url);
+  }
+
+  public PImageFuture getImageFuture( URL url )
+  {
+    return getImageFuture(url, -1, -1);
+  }
+
+  public PImageFuture getImageFuture( URL url, int width, int height )
+  {
+    return new PImageFuture(this, getImage(url), width, height);
   }
 }
