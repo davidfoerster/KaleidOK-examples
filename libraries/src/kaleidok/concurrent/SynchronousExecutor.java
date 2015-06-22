88da983b3503528c7b6be6b9d05573ef2a49fd8c
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-06-22 17:18:49 +0200

    Synchonous Executor implementation
    can be used for debugging

---
+++ libraries/src/kaleidok/concurrent/SynchronousExecutor.java
@@ -0,0 +1,26 @@
+package kaleidok.concurrent;
+
+import java.util.concurrent.Executor;
+
+
+/**
+ * This <code>Executor</code> implementation executes submitted tasks
+ * synchronously upon submission in the submitting thread.
+ * <p>
+ * A possible use case is testing and debugging of task or event driven code.
+ */
+public class SynchronousExecutor implements Executor
+{
+  public static final SynchronousExecutor INSTANCE =
+    new SynchronousExecutor();
+
+
+  protected SynchronousExecutor() { }
+
+
+  @Override
+  public void execute( Runnable command )
+  {
+    command.run();
+  }
+}
