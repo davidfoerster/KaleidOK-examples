2680f1e13fef07396a235f71c8fb65f0f762c0a4
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-06-28 12:30:25 +0200

    Pass display device to fullscreen event handler

--- libraries/src/kaleidok/swing/FullscreenEventListener.java
+++ libraries/src/kaleidok/swing/FullscreenEventListener.java
@@ -1,10 +1,12 @@
 package kaleidok.swing;
 
+import java.awt.GraphicsDevice;
 import java.awt.Window;
 import java.util.EventListener;
 
 
 public interface FullscreenEventListener extends EventListener
 {
-  void handleFullscreenStateChange( Window w, boolean fullscreen );
+  void handleFullscreenStateChange( GraphicsDevice device, Window w,
+    boolean fullscreen );
 }
