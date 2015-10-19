719b6c9d336956213c1a630457b695af2ee57c89
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-10-19 21:03:58 +0200

    STT-Mock: Bind to arbitrary free port on loopback interface

--- libraries/src/kaleidok/google/speech/mock/MockTranscriptionService.java
+++ libraries/src/kaleidok/google/speech/mock/MockTranscriptionService.java
@@ -7,6 +7,7 @@ import com.sun.net.httpserver.HttpServer;
 import org.apache.http.concurrent.FutureCallback;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -16,30 +17,55 @@ import java.util.logging.Logger;
 
 public class MockTranscriptionService extends TranscriptionService
 {
-  public static final URL MOCK_API_BASE;
-  static {
-    try {
-      MOCK_API_BASE = new URL(
-        "http", "localhost", 8081, DEFAULT_API_BASE.getPath());
-    } catch (MalformedURLException ex) {
-      throw new AssertionError(ex);
-    }
-  }
+  static final Logger logger =
+    Logger.getLogger(MockTranscriptionService.class.getPackage().getName());
+
+  private static URL MOCK_API_BASE = null;
 
   private static HttpServer server = null;
 
   private static MockSpeechToTextHandler sttHandler;
 
 
-  protected static void initServer( STT stt ) throws IOException
+  protected static void initServer( STT stt, InetSocketAddress addr ) throws IOException
   {
+    boolean createNew = false;
     if (server == null) {
-      sttHandler =  new MockSpeechToTextHandler(stt);
-      server = HttpServer.create(new InetSocketAddress(MOCK_API_BASE.getPort()), 0);
-      server.createContext(MOCK_API_BASE.getPath(), sttHandler);
-      server.start();
+      synchronized (MockTranscriptionService.class) {
+        if (server == null) {
+          server = HttpServer.create(addr, 0);
+          sttHandler = new MockSpeechToTextHandler(stt);
+          server.createContext(DEFAULT_API_BASE.getPath(), sttHandler);
+          server.start();
+          addr = server.getAddress();
+          try {
+            MOCK_API_BASE = new URL(
+              "http", addr.getHostString(), addr.getPort(),
+              DEFAULT_API_BASE.getPath());
+          } catch (MalformedURLException ex) {
+            throw new AssertionError(ex);
+          }
+          createNew = true;
+        }
+      }
+    }
+    if (createNew) {
+      logger.log(Level.FINER, "Running {0} at {1}",
+        new Object[]{MockTranscriptionService.class.getSimpleName(), MOCK_API_BASE});
     } else {
-      assert stt == sttHandler.stt;
+      assert stt == sttHandler.stt && server.getAddress().equals(addr);
+    }
+  }
+
+
+  private static URL initServerThrowError( STT stt, String hostName, int port )
+  {
+    try {
+      InetAddress hostAddr = InetAddress.getByName(hostName);
+      initServer(stt, new InetSocketAddress(hostAddr, port));
+      return MOCK_API_BASE;
+    } catch (IOException ex) {
+      throw new Error(ex);
     }
   }
 
@@ -47,15 +73,10 @@ public class MockTranscriptionService extends TranscriptionService
   public MockTranscriptionService( String accessKey,
     FutureCallback<SttResponse> resultHandler, STT stt )
   {
-    super(MOCK_API_BASE, accessKey, resultHandler);
-    try {
-      initServer(stt);
-    } catch (IOException ex) {
-      throw new Error(ex);
-    }
+    super(initServerThrowError(stt, null, 0), accessKey, resultHandler);
 
     String className = this.getClass().getCanonicalName();
-    Logger.getLogger(className).log(Level.CONFIG,
+    logger.log(Level.CONFIG,
       "You set your Google API access key to \"{0}\"; " +
         "speech transcription is performed by {1}",
       new Object[]{accessKey, className});
