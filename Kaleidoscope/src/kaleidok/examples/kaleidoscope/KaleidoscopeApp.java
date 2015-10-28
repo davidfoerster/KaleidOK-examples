59c952cefab9fddab29f57201a50064f3ef12953
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-10-28 08:55:00 +0100

    Kaleidoscope: Place control window next to sketch window
    Screen size determines, whether it'll be moved to the right or below or
    not at all.

--- Kaleidoscope/src/kaleidok/examples/kaleidoscope/KaleidoscopeApp.java
+++ Kaleidoscope/src/kaleidok/examples/kaleidoscope/KaleidoscopeApp.java
@@ -7,6 +7,7 @@ import kaleidok.processing.ProcessingSketchAppletWrapper;
 import kaleidok.util.AssertionUtils;
 
 import javax.swing.JApplet;
+import java.awt.*;
 import java.io.IOException;
 
 
@@ -43,6 +44,16 @@ public class KaleidoscopeApp extends ProcessingSketchAppletWrapper<Kaleidoscope>
   {
     if (controls == null) {
       controls = new KaleidoscopeControls(this);
+
+      Window w = getRootPane().getTopLevelWindow();
+      Rectangle screen = w.getGraphicsConfiguration().getBounds();
+      int x = w.getX() + w.getWidth() + 5,
+        y = w.getY() + w.getHeight() + 5;
+      if (screen.x + screen.width >= x + controls.getWidth()) {
+        controls.setLocation(x, w.getY());
+      } else if (screen.y + screen.height >= y + controls.getHeight()) {
+        controls.setLocation(w.getX(), y);
+      }
     }
     return controls;
   }
