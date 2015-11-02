da986bb95c2b6325e351c83528385a470057dd42
David Foerster <david.foerster@informatik.hu-berlin.de>
2015-11-02 03:03:29 +0100

    Replay pre-recorded interactions from audio file sequence

--- Kaleidoscope/src/kaleidok/examples/kaleidoscope/AudioProcessingManager.java
+++ Kaleidoscope/src/kaleidok/examples/kaleidoscope/AudioProcessingManager.java
@@ -1,17 +1,30 @@
 package kaleidok.examples.kaleidoscope;
 
 import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.io.TarsosDSPAudioInputStream;
 import be.tarsos.dsp.io.jvm.AudioPlayer;
 import be.tarsos.dsp.io.jvm.JVMAudioInputStream;
+import com.google.gson.JsonParseException;
 import kaleidok.audio.ContinuousAudioInputStream;
 import kaleidok.audio.DummyAudioPlayer;
+import kaleidok.audio.MultiAudioInputStream;
 import kaleidok.audio.processor.MinimFFTProcessor;
 import kaleidok.audio.processor.VolumeLevelProcessor;
+import kaleidok.google.gson.TypeAdapterManager;
 import kaleidok.util.DefaultValueParser;
+import processing.event.KeyEvent;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.logging.Level;
 
 import static be.tarsos.dsp.io.jvm.AudioDispatcherFactory.fromDefaultMicrophone;
 import static kaleidok.examples.kaleidoscope.Kaleidoscope.logger;
@@ -88,18 +101,33 @@ public class AudioProcessingManager
       try {
         if (audioSource == null)
         {
+
           audioDispatcher =
             fromDefaultMicrophone(sampleRate, bufferSize, bufferOverlap);
           dispatcherRunnable = audioDispatcher;
         }
         else
         {
-          audioDispatcher =
-            new AudioDispatcher(new ContinuousAudioInputStream(audioSource),
-              bufferSize, bufferOverlap);
+          TarsosDSPAudioInputStream ais;
+          if (audioSource.endsWith(".json")) {
+            replayAction = new ReplayAction(audioSource);
+            ais = replayAction.audioInputStream;
+            dispatcherRunnable = new Runnable()
+              {
+                @Override
+                public void run()
+                {
+                  replayAction.doReplayItem(0);
+                  audioDispatcher.run();
+                }
+              };
+          } else {
+            ais = new ContinuousAudioInputStream(audioSource);
+          }
+          audioDispatcher = new AudioDispatcher(ais, bufferSize, bufferOverlap);
           dispatcherRunnable = initAudioPlayer(audioDispatcher);
         }
-      } catch (IOException | UnsupportedAudioFileException | LineUnavailableException ex) {
+      } catch (IOException | UnsupportedAudioFileException | LineUnavailableException | JsonParseException ex) {
         throw new Error(ex);
       }
 
@@ -179,5 +207,163 @@ public class AudioProcessingManager
     if (fftProcessor == null)
       fftProcessor = new MinimFFTProcessor(getAudioBufferSize());
     return fftProcessor;
+  }
+
+
+  private ReplayAction replayAction = null;
+
+
+  public class ReplayAction implements ActionListener
+  {
+    public final ReplayList replayList;
+
+    public final MultiAudioInputStream audioInputStream;
+
+    public final Timer timer;
+
+    private volatile int idx = -1;
+
+    private Boolean ignoreTranscriptionResult = null;
+
+
+    private ReplayAction( String filename )
+      throws IOException, JsonParseException, UnsupportedAudioFileException
+    {
+      this(TypeAdapterManager.getGson().fromJson(
+        new FileReader(filename), ReplayList.class));
+    }
+
+
+    private ReplayAction( ReplayList replayList )
+      throws IOException, JsonParseException, UnsupportedAudioFileException
+    {
+      if (replayList.items.length == 0)
+        throw new NoSuchElementException("Empty replay list");
+
+      audioInputStream = new MultiAudioInputStream(
+        new ArrayList<TarsosDSPAudioInputStream>(replayList.items.length));
+
+      for (ReplayList.Item item: replayList.items) {
+        audioInputStream.streams.add(
+          new ContinuousAudioInputStream(new URL(item.url).openStream()));
+      }
+
+      if (!audioInputStream.isFormatCompatible(
+        audioInputStream.streams.get(0).getFormat()))
+      {
+        throw new UnsupportedAudioFileException("Mismatching audio formats");
+      }
+
+      parent.registerMethod("keyEvent", this);
+      timer = new Timer(0, this);
+      timer.setRepeats(false);
+      this.replayList = replayList;
+
+      logger.log(Level.CONFIG,
+        "Replaying recorded interaction \"{0}\"", replayList.name);
+    }
+
+
+    public void keyEvent( KeyEvent ev )
+    {
+      if (ev.getAction() == KeyEvent.TYPE) {
+        switch (ev.getKey()) {
+        case 'n':
+          doNextReplayItem();
+          break;
+        }
+      }
+    }
+
+
+    private boolean checkRunning()
+    {
+      if (timer.isRunning()) {
+        System.err.println("Please wait for the current replay action to finish!");
+        return true;
+      }
+      return false;
+    }
+
+
+    public void doNextReplayItem()
+    {
+      if (checkRunning())
+        return;
+
+      MultiAudioInputStream ais = audioInputStream;
+      int idx = ais.getCurrentIdx() + 1;
+      if (idx >= ais.streams.size()) {
+        logger.info("Last replay item has been finished");
+        return;
+      }
+
+      doReplayItem(idx);
+    }
+
+
+    public void doReplayItem( int idx )
+    {
+      if (checkRunning())
+        return;
+
+      try {
+        audioInputStream.setCurrentIdx(idx);
+      } catch (IOException ex) {
+        logger.log(Level.SEVERE, "Couldn't skip to next replay item", ex);
+        return;
+      }
+      this.idx = idx;
+      doReplayItem();
+    }
+
+
+    public void doReplayItem()
+    {
+      if (checkRunning())
+        return;
+
+      double streamLength =
+        audioInputStream.getCurrentStream().getFrameLength() /
+          (double) audioInputStream.getFormat().getSampleRate();
+      timer.setInitialDelay((int)(streamLength * 1e3));
+      timer.start();
+    }
+
+
+    @Override
+    public void actionPerformed( ActionEvent ev )
+    {
+      if (!isIgnoreTranscriptionResult()) {
+        parent.getChromasthetiationService()
+          .submit(replayList.items[idx].transcription);
+      }
+
+      doNextReplayItem();
+    }
+
+
+    private boolean isIgnoreTranscriptionResult()
+    {
+      if (ignoreTranscriptionResult == null) {
+        ignoreTranscriptionResult =
+          SttManager.getParamIgnoreTranscriptionResult(parent);
+      }
+      return ignoreTranscriptionResult;
+    }
+  }
+
+
+  private static class ReplayList
+  {
+    public String name;
+
+    public Item[] items;
+
+
+    public static class Item
+    {
+      public String url, transcription;
+    }
   }
 }
