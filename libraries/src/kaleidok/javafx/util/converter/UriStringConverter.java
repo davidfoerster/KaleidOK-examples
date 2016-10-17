2a980d3719bd6bbd063cc6268cec35b097d3da28
David Foerster <david.foerster@informatik.hu-berlin.de>
2016-10-18 00:34:12 +0200

    StringConverter for URI objects

---
+++ libraries/src/kaleidok/javafx/util/converter/UriStringConverter.java
@@ -0,0 +1,33 @@
+package kaleidok.javafx.util.converter;
+
+import javafx.util.StringConverter;
+
+import java.net.URI;
+
+
+public class UriStringConverter extends StringConverter<URI>
+{
+  public static final UriStringConverter INSTANCE = new UriStringConverter();
+
+
+  protected UriStringConverter() { }
+
+
+  @Override
+  public String toString( URI object )
+  {
+    return (object != null) ? object.toString() : null;
+  }
+
+
+  @Override
+  public URI fromString( String string )
+  {
+    URI value = URI.create(string);
+
+    if (!value.isAbsolute() || value.isOpaque())
+      throw new IllegalArgumentException("relative or opaque URI");
+
+    return value;
+  }
+}
