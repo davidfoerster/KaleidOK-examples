1691936531b1cfbeeacb196245cb11ad1a722aaa
David Foerster <david.foerster@informatik.hu-berlin.de>
2016-10-22 21:48:23 +0200

    Track current EditorNodeInfo in EditableTreeTableCell more intelligently

--- libraries/src/kaleidok/javafx/scene/control/cell/EditableTreeTableCell.java
+++ libraries/src/kaleidok/javafx/scene/control/cell/EditableTreeTableCell.java
@@ -12,31 +12,41 @@ import java.util.function.BiConsumer;
 public class EditableTreeTableCell<T, N extends Node>
   extends TreeTableCell<ReadOnlyProperty<T>, T>
 {
-  private EditorNodeInfo<N, T> getEditorNodeInfo()
-  {
-    if (isEditableInherited())
-    {
-      TreeItem<ReadOnlyProperty<T>> item = getTreeTableRow().getTreeItem();
-      if (item instanceof EditableTreeItem)
-      {
-        EditorNodeInfo<N, T> editorNodeInfo =
-          ((EditableTreeItem<T, N>) item).getEditorNodeInfo();
-        if (editorNodeInfo != null)
-        {
-          if (!editorNodeInfo.isEmpty())
-            return editorNodeInfo;
+  private EditorNodeInfo<N, T> nodeInfo = null;
 
-          setEditable(false);
+
+  private void updateNodeInfo( TreeItem<ReadOnlyProperty<T>> item )
+  {
+    if (item instanceof EditableTreeItem && isEditableInherited())
+    {
+      EditorNodeInfo<N, T> nodeInfo =
+        ((EditableTreeItem<T, N>) item).getEditorNodeInfo();
+      if (nodeInfo != null)
+      {
+        if (!nodeInfo.isEmpty())
+        {
+          this.nodeInfo = nodeInfo;
+          return;
         }
+
+        setEditable(false);
       }
     }
-    return null;
+    this.nodeInfo = null;
+  }
+
+
+  @Override
+  public void updateIndex( int i )
+  {
+    updateNodeInfo(getTreeTableView().getTreeItem(i));
+    super.updateIndex(i);
   }
 
 
   public N getEditorNode()
   {
-    EditorNodeInfo<N, ?> nodeInfo = getEditorNodeInfo();
+    EditorNodeInfo<N, ?> nodeInfo = this.nodeInfo;
     return (nodeInfo != null) ? nodeInfo.node : null;
   }
 
@@ -44,7 +54,7 @@ public class EditableTreeTableCell<T, N extends Node>
   private BiConsumer<? super EditableTreeTableCell<? super T, N>, ? super T>
   getValueChangeListener()
   {
-    EditorNodeInfo<N, T> nodeInfo = getEditorNodeInfo();
+    EditorNodeInfo<N, T> nodeInfo = this.nodeInfo;
     return (nodeInfo != null) ? nodeInfo.valueChange : null;
   }
 
@@ -66,7 +76,7 @@ public class EditableTreeTableCell<T, N extends Node>
     if (item == null || empty)
       return null;
 
-    EditorNodeInfo<?, T> nodeInfo = getEditorNodeInfo();
+    EditorNodeInfo<?, T> nodeInfo = this.nodeInfo;
     return (nodeInfo != null && nodeInfo.converter != null) ?
       nodeInfo.converter.toString(item) :
       item.toString();
