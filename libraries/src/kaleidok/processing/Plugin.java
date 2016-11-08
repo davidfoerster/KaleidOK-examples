715fc1b8d9db8126dc41084a10901defe6cff744
David Foerster <david.foerster@informatik.hu-berlin.de>
2016-11-08 16:38:10 +0100

    Support methods for classes classes hooking into PApplet

--- libraries/src/kaleidok/processing/Plugin.java
+++ libraries/src/kaleidok/processing/Plugin.java
@@ -1,5 +1,6 @@
 package kaleidok.processing;
 
+import kaleidok.util.Reflection;
 import processing.core.PApplet;
 import processing.event.KeyEvent;
 import processing.event.MouseEvent;
@@ -8,7 +9,11 @@ import processing.event.TouchEvent;
 import javax.annotation.OverridingMethodsMustInvokeSuper;
 import java.lang.reflect.Method;
 import java.util.EnumSet;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.apache.commons.lang3.ArrayUtils.EMPTY_CLASS_ARRAY;
 
@@ -17,15 +22,15 @@ public class Plugin<P extends PApplet>
 {
   public enum HookMethod {
     pre, draw, post, pause, resume, dispose,
-    mouseEvent(new Class<?>[]{ MouseEvent.class }),
-    keyEvent(new Class<?>[]{ KeyEvent.class }),
-    touchEvent(new Class<?>[]{ TouchEvent.class });
+    mouseEvent(MouseEvent.class),
+    keyEvent(KeyEvent.class),
+    touchEvent(TouchEvent.class);
 
 
     private final Class<?>[] argumentTypes;
 
 
-    HookMethod( Class<?>[] argumentTypes )
+    HookMethod( Class<?>... argumentTypes )
     {
       this.argumentTypes = Objects.requireNonNull(argumentTypes);
     }
@@ -60,6 +65,87 @@ public class Plugin<P extends PApplet>
     {
       return getMethodReference(plugin).getDeclaringClass() != Plugin.class;
     }
+
+
+    static final EnumSet<HookMethod> valueSet =
+      EnumSet.allOf(HookMethod.class);
+
+    static final Map<String, HookMethod> valueMap =
+      valueSet.stream().collect(
+        Collectors.toMap(Enum::name, Function.identity()));
+
+
+    public static EnumSet<HookMethod> getAvailableHooks( Plugin<?> plugin )
+    {
+      EnumSet<HookMethod> result = EnumSet.noneOf(HookMethod.class);
+      for (HookMethod hm: valueSet)
+      {
+        if (hm == dispose || hm.isOverriddenOn(plugin))
+          result.add(hm);
+      }
+      return result;
+    }
+
+
+    public static EnumSet<HookMethod> getAvailableHooks( Object plugin )
+    {
+      if (plugin instanceof Plugin)
+        return getAvailableHooks((Plugin<?>) plugin);
+
+      EnumSet<HookMethod> result = EnumSet.noneOf(HookMethod.class);
+      outer:
+      for (Method m : plugin.getClass().getMethods())
+      {
+        HookMethod hm = valueMap.get(m.getName());
+        if (hm != null && m.getParameterCount() == hm.argumentTypes.length)
+        {
+          Class<?>[] expectedArgumentTypes = hm.argumentTypes;
+          if (expectedArgumentTypes.length != 0)
+          {
+            Class<?>[] actualArgumentTypes = m.getParameterTypes();
+            for (int i = expectedArgumentTypes.length - 1; i >= 0; i--)
+            {
+              if (!Reflection.getWrapperType(expectedArgumentTypes[i])
+                .isAssignableFrom(
+                  Reflection.getWrapperType(actualArgumentTypes[i])))
+              {
+                continue outer;
+              }
+            }
+          }
+          result.add(hm);
+        }
+      }
+      return result;
+    }
+
+
+    public static void register( PApplet applet, Object plugin,
+      Set<HookMethod> methods )
+    {
+      for (HookMethod m: methods)
+        applet.registerMethod(m.name(), plugin);
+    }
+
+
+    public static void registerAll( PApplet applet, Object plugin )
+    {
+      register(applet, plugin, getAvailableHooks(plugin));
+    }
+
+
+    public static void unregister( PApplet applet, Object plugin,
+      Set<HookMethod> methods )
+    {
+      for (HookMethod m: methods)
+        applet.unregisterMethod(m.name(), plugin);
+    }
+
+
+    public static void unregisterAll( PApplet applet, Object plugin )
+    {
+      unregister(applet, plugin, getAvailableHooks(plugin));
+    }
   }
 
 
@@ -78,7 +164,7 @@ public class Plugin<P extends PApplet>
 
   private void registerHooks()
   {
-    for (HookMethod method: HookMethod.values())
+    for (HookMethod method: HookMethod.valueSet)
     {
       if (method == HookMethod.dispose || method.isOverriddenOn(this))
         registerHook(method);
