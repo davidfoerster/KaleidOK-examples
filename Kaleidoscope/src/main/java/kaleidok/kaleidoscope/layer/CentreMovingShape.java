davidfoerster/KaleidOK-examples

commit 04d3339a5fe75813831a22e3cc79e300fd73284c

Various minor improvements

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/CentreMovingShape.java
@@ -160,9 +160,11 @@ public class CentreMovingShape extends CircularImageLayer
 
     final int segmentCount = this.segmentCount.get();
     parent.vertex(0, 0, 0.5f, 0.5f); // define a central point for the TRIANGLE_FAN, note the (0.5, 0.5) uv texture coordinates
-    for (int i = 0; i <= segmentCount; i++) {
-      drawCircleVertex(i % segmentCount, radius);
+    for (int i = 0; i < segmentCount; i++) {
+      drawCircleVertex(i, radius);
     }
+    drawCircleVertex(segmentCount, radius);
+
     parent.endShape(); // finalize the Shape
     parent.popMatrix(); // use push/popMatrix so each Shape's translation does not affect other drawings
   }

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/OuterMovingShape.java
@@ -209,9 +209,11 @@ public class OuterMovingShape extends CircularImageLayer
 
     final int segmentCount = this.segmentCount.get();
     parent.vertex(0, 0, 0.5f, 0.5f); // define a central point for the TRIANGLE_FAN, note the (0.5, 0.5) uv texture coordinates
-    for (int i = 0; i <= segmentCount; i++) {
-      drawCircleVertex(i % segmentCount, 1);
+    for (int i = 0; i < segmentCount; i++) {
+      drawCircleVertex(i, 1);
     }
+    drawCircleVertex(segmentCount, 1);
+
     parent.endShape(); // finalize the Shape
     parent.popMatrix(); // use push/popMatrix so each Shape's translation does not affect other drawings
   }

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/ImageLayer.java
@@ -122,8 +122,7 @@ public abstract class ImageLayer implements Runnable, PreferenceBean
    * Updates and retrieves the current image for this layer. If the
    * {@link Future} returned by {@link #getNextImage()} is
    * {@link Future#isDone() done} and, its wrapped value is used as the
-   * new
-   * current image and the next image is reset as if calling
+   * new current image and the next image is reset as if calling
    * {@link #setNextImage(Future) setNextImage(null)}.
    * <p>
    * {@link Future#isCancelled() Cancelled} futures are skipped silently.

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/SpectrogramLayer.java
@@ -58,7 +58,8 @@ public class SpectrogramLayer extends CircularImageLayer
 
     exponent = new AspectedDoubleProperty(this, "exponent", 1.125);
     exponent
-      .addAspect(BoundedDoubleTag.getDoubleInstance(), new DoubleSpinnerValueFactory(0, 4))
+      .addAspect(BoundedDoubleTag.getDoubleInstance(),
+        new DoubleSpinnerValueFactory(0, 4))
       .setAmountToStepBy(0.05);
     exponent.addAspect(PropertyPreferencesAdapterTag.getInstance());
   }
@@ -135,8 +136,10 @@ public class SpectrogramLayer extends CircularImageLayer
       scaleFactor = this.scaleFactor.floatValue();
     final double exponent = this.exponent.get();
     final int segmentCount = this.segmentCount.get();
+    /*
     assert segmentCount <= avgSpectrum.size() :
       segmentCount + " > " + avgSpectrum.size();
+    */
 
     parent.pushMatrix(); // use push/popMatrix so each Shape's translation does not affect other drawings
     parent.scale(outerRadius);

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/util/CircularTriangleStripSegmentCoordinatesBinding.java
@@ -3,6 +3,8 @@ package kaleidok.kaleidoscope.layer.util;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.value.ObservableIntegerValue;
 
+import java.util.Objects;
+
 
 public class CircularTriangleStripSegmentCoordinatesBinding
   extends ObjectBinding<float[]>
@@ -20,8 +22,7 @@ public class CircularTriangleStripSegmentCoordinatesBinding
   public CircularTriangleStripSegmentCoordinatesBinding(
     ObservableIntegerValue segmentCount )
   {
-    bind(segmentCount);
-    this.segmentCount = segmentCount;
+    bind(this.segmentCount = Objects.requireNonNull(segmentCount));
   }
 
 

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/util/SpectrumBandsPerOctaveBinding.java
@@ -27,8 +27,7 @@ public class SpectrumBandsPerOctaveBinding extends IntegerBinding
     if (sampleRate <= 0 || !Double.isFinite(sampleRate))
       throw new IllegalArgumentException("non-positive or non-finite sample rate");
 
-    bind(totalBands);
-    this.totalBands = totalBands;
+    bind(this.totalBands = Objects.requireNonNull(totalBands));
     octaveCount = log2(sampleRate * (0.5 / MIN_FREQUENCY));
   }
 

libraries/src/main/java/kaleidok/javafx/beans/property/binding/MessageFormatBinding.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.text.FieldPosition;
 import java.text.MessageFormat;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 
@@ -24,8 +25,7 @@ public class MessageFormatBinding
 
   public MessageFormatBinding( ObservableValue<String> formatString )
   {
-    this.formatString = formatString;
-    bind(formatString);
+    bind(this.formatString = Objects.requireNonNull(formatString));
   }
 
 

commit aee3c8d19acdd176332c9b8a41c85caa410d514e

Fix bugs/typos introduced in 04d3339a5fe75813831a22e3cc79e300fd73284c

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/CentreMovingShape.java
@@ -163,7 +163,7 @@ public class CentreMovingShape extends CircularImageLayer
     for (int i = 0; i < segmentCount; i++) {
       drawCircleVertex(i, radius);
     }
-    drawCircleVertex(segmentCount, radius);
+    drawCircleVertex(0, radius);
 
     parent.endShape(); // finalize the Shape
     parent.popMatrix(); // use push/popMatrix so each Shape's translation does not affect other drawings

Kaleidoscope/src/main/java/kaleidok/kaleidoscope/layer/OuterMovingShape.java
@@ -212,7 +212,7 @@ public class OuterMovingShape extends CircularImageLayer
     for (int i = 0; i < segmentCount; i++) {
       drawCircleVertex(i, 1);
     }
-    drawCircleVertex(segmentCount, 1);
+    drawCircleVertex(0, 1);
 
     parent.endShape(); // finalize the Shape
     parent.popMatrix(); // use push/popMatrix so each Shape's translation does not affect other drawings